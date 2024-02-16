1c87a9b7fed93b483d34b75bcd7ae4f06dd29f21
bugyepy <blue.bugyep@gmail.com>
2024-02-16 21:22:36 +0900

    init: 作った

--- app/page.tsx
+++ app/page.tsx
@@ -1,112 +1,137 @@
+"use client";
+
+import { useRef, useState } from "react";
+import dynamic from "next/dynamic";
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogDescription,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+const Canvas = dynamic(() => import("../components/ui/canvas"), {
+  ssr: false,
+});
 
 export default function Home() {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [isOpen, setIsOpen] = useState(false);
+  const [image, setImage] = useState<File>();
+  const [pestedUrl, setPestedUrl] = useState<string | null>(null);
+
+  const handlePestedUrlChange = (pestedUrl: string) => {
+    setPestedUrl(pestedUrl);
+  };
+
+  const handleInput = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    if (!e.target.files) return;
+    setImage(e.target.files[0]);
+    setIsOpen(true);
+  };
+
+  const handleOpenChange = (bool: Boolean) => {
+    if (!bool) handleClose();
+    if (bool && !image) setIsOpen(false);
+    if (bool && image) {
+      setIsOpen(true);
+    }
+  };
+
+  const handleClose = () => {
+    setIsOpen(false);
+    setImage(undefined);
+    setPestedUrl(null)
+
+    if (inputRef.current) {
+      inputRef.current.value = "";
+    }
+  };
+
+  const handleSave = () => {
+    if (!pestedUrl) return;
+
+    const link = document.createElement("a");
+    link.download = "pestedImage.png";
+    link.href = pestedUrl;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+
+    setPestedUrl(null)
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <main className="min-h-screen p-24">
+      <h1 className="pb-2 text-4xl font-bold">
+        Pseudes - Pesting for quickly.
+      </h1>
+      <h2 className="pb-8 text-xl font-semibold">
+        安心で安全。完全に無料。サーバに画像は保存されません。
+      </h2>
+      <Dialog open={isOpen} onOpenChange={handleOpenChange}>
+        <DialogTrigger asChild>
+          <label className="block">
+            <input
+              type="file"
+              className="block w-full text-sm text-slate-500 file:mr-2 file:rounded-full file:border-0 file:bg-violet-50 file:px-4 file:py-2 file:text-sm file:font-semibold file:text-violet-700 hover:file:bg-violet-100"
+              id="image"
+              accept="image/*"
+              placeholder="画像"
+              onInput={handleInput}
+              ref={inputRef}
             />
-          </a>
-        </div>
-      </div>
+          </label>
+        </DialogTrigger>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>画像を偽物にする</DialogTitle>
+            <DialogDescription>
+              画像に合わせてウォーターマークを移動させてください。
+            </DialogDescription>
+          </DialogHeader>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          {image && (
+            <Canvas
+              image={image}
+              handlePestedUrlChange={handlePestedUrlChange}
+            />
+          )}
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+          <DialogFooter className="gap-y-4">
+            <Button type="button" variant="outline" onClick={handleClose}>
+              キャンセル
+            </Button>
+            <Button type="button" onClick={handleSave} disabled={!pestedUrl}>
+              保存する
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div
+        className="mt-8"
+        style={{ position: "relative", width: "100%", height: "600px" }}
+      >
+        {!isOpen && image && (
+          <Image
+            src={URL.createObjectURL(image)}
+            alt={"ファイルを選択してください。"}
+            fill
+            style={{
+              objectFit: "contain",
+              objectPosition: "0 0",
+              top: "0",
+              left: "0",
+            }}
+          />
+        )}
       </div>
     </main>
   );
---
+++ components/ui/canvas.tsx
@@ -0,0 +1,114 @@
+import Konva from "konva";
+import { useEffect, useRef, useState } from "react";
+import { Stage, Layer, Image, Circle, Text, Rect } from "react-konva";
+import useImage from "use-image";
+
+type ImageProps = {
+  image: File;
+  handlePestedUrlChange: (pestedUrl: string) => void;
+};
+
+const CanvasComponent = ({ image, handlePestedUrlChange }: ImageProps) => {
+  const [imageUrl, setImageUrl] = useState("");
+  const [canvasWidth, setCanvasWidth] = useState<number>(375);
+  const [canvasHeight, setCanvasHeight] = useState<number>(375);
+  const [srcWidth, setSrcWidth] = useState<number | null>(null);
+  const [srcHeight, setSrcHeight] = useState<number | null>(null);
+  const [width, setWidth] = useState(1);
+  const [height, setHeight] = useState(1);
+  const stageRef = useRef<Konva.Stage>(null);
+  const [isGuideView, seIisGuideView] = useState(true);
+
+  useEffect(() => {
+    if (!image) return;
+
+    const url = URL.createObjectURL(image);
+    setImageUrl(url);
+  }, [image]);
+
+  const [imageData] = useImage(imageUrl);
+
+  // 画像サイズの取得と変更
+  useEffect(() => {
+    setSrcWidth(imageData?.width ?? null);
+    setSrcHeight(imageData?.height ?? null);
+
+    if (!(srcWidth && srcHeight)) return;
+
+    // 375*375の枠内に収まるように画像サイズを変更する
+    if (1 > srcWidth / srcHeight) {
+      setWidth(srcWidth * (375 / srcHeight));
+      setCanvasWidth(srcWidth * (375 / srcHeight));
+      setHeight(srcHeight * (375 / srcHeight));
+      setCanvasHeight(srcHeight * (375 / srcHeight));
+    } else {
+      setWidth(srcWidth * (375 / srcWidth));
+      setCanvasWidth(srcWidth * (375 / srcWidth));
+      setHeight(srcHeight * (375 / srcWidth));
+      setCanvasHeight(srcHeight * (375 / srcWidth));
+    }
+  }, [imageData?.width, imageData?.height, srcWidth, srcHeight, width]);
+
+  useEffect(() => {
+    if (isGuideView || !stageRef.current) return;
+
+    handlePestedUrlChange(stageRef.current.toDataURL());
+  }, [handlePestedUrlChange, isGuideView, stageRef]);
+
+  const handleDragStart = () => {
+    seIisGuideView(false);
+
+  };
+
+  const handleDragEnd = () => {
+    if (stageRef.current) {
+      handlePestedUrlChange(stageRef.current.toDataURL());
+    };
+
+    if (isGuideView) {
+      seIisGuideView(false);
+      return;
+    }
+
+    seIisGuideView(true);
+  };
+
+  return (
+    <Stage width={canvasWidth} height={canvasHeight} ref={stageRef}>
+      <Layer>
+        <Image image={imageData} width={width} height={height} alt="" />
+      </Layer>
+      <Layer
+        draggable={true}
+        onDragStart={handleDragStart}
+        onDragEnd ={handleDragEnd}
+      >
+        {isGuideView && (
+          <Rect
+            stroke="violet"
+            strokeWidth={3}
+            x={5}
+            y={5}
+            width={195}
+            height={50}
+            opacity={0.6}
+          />
+        )}
+
+        <Circle fill="white" x={20} y={20} radius={7.5} />
+        <Circle stroke="white" strokeWidth={2} x={40} y={20} radius={7} />
+        <Circle stroke="white" strokeWidth={2} x={60} y={20} radius={7} />
+        <Text
+          text={"SHOT ON TAKANASHI G5"}
+          x={13}
+          y={32}
+          fontSize={18}
+          fontFamily={"Calibri"}
+          fill="white"
+        />
+      </Layer>
+    </Stage>
+  );
+};
+
+export default CanvasComponent;
